a778d9f9dd788fbed030e8f156f0f82b4b9dc8a0
victoriiapashynska <victoriapashynska@gmail.com>
2023-11-11 13:00:14 -0500

    Solved 3 Tasks

---
+++ src/EmmaTasks/GroupStudying/Task2_DivideWithout.java
@@ -0,0 +1,29 @@
+package EmmaTasks.GroupStudying;
+
+public class Task2_DivideWithout {
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 3;
+
+        divide(num1,num2);
+    }
+
+    public static void divide(int n1, int n2){
+
+        // 5*4 =20 --> 5+5+5+5 = 20
+        // 20 = 20-5-5-5-5 = 0
+        // n1=20 n2=3 6.6666
+
+        int remainder = n1 % n2;
+        int result =0;
+
+        while(n1>=n2){
+            n1 = n1 - n2;
+            result++;
+        }
+        System.out.println(result +"." +remainder);
+
+    }
+
+}
