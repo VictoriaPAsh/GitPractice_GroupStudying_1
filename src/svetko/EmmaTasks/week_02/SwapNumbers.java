f699041715fd2574f5bf4eeb35aef44c91d3b575
svetko <ssadmitrovic@gmail.com>
2023-11-20 21:40:24 -0500

    Emma tasks week_02 updated

---
+++ src/svetko/EmmaTasks/week_02/SwapNumbers.java
@@ -0,0 +1,18 @@
+package svetko.EmmaTasks.week_02;
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int x = 15;
+        int y = 20;
+
+        System.out.println("x = " + y);
+        System.out.println("y = " + x);
+
+
+    }
+}
+/*
+Swap two variables without using third variable
+ */
