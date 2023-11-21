1d11c3ea42b0e28b950cb57daf800650df9171e8
dianaltb <dianaaltunbas@gmail.com>
2023-11-20 23:59:09 -0500

    Created OddOrEven class

---
+++ src/EmmaTasks/Diana/week1/OddOrEven.java
@@ -0,0 +1,20 @@
+package EmmaTasks.Diana.week1;
+
+public class OddOrEven {
+
+        public static void main(String[] args) {
+
+            System.out.println(oddOrEven(10));
+
+        }
+        public static String oddOrEven(int num){
+            String result = "";
+            if (num % 2 ==0){
+                result = "Even";
+            } else{
+                result = "Odd";
+            }
+            return result;
+
+        }
+    }
