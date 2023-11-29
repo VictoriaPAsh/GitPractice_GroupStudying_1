856204c36e1baba507040b87fa13f49666581ffc
Tugbabaki <tugbabakiki@gmail.com>
2023-11-29 00:53:54 -0600

    commiting  after adding a 2nd solution to task 7 and making little changes to task 8

--- src/EmmaTasks/GroupStudying/tubish/Task7_Reverse_Negative_Num.java
+++ src/EmmaTasks/GroupStudying/tubish/Task7_Reverse_Negative_Num.java
@@ -1,25 +1,42 @@
 package EmmaTasks.GroupStudying.tubish;
 
+import java.util.Scanner;
+
 public class Task7_Reverse_Negative_Num {
 
     public static void main(String[] args) {
-        System.out.println(ReverseNegative(-5432));
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a negative number:");
+        int num = in.nextInt();
+        System.out.println(reverseNegativeNum(num));
+
+        System.out.println(reverseNegative(num));
     }
 
-    public static int ReverseNegative(int n) {
-        if (!(n <= 0)) {  // -5432  %10 == last digit. we have -2
+
+    public static int reverseNegative(int n) {
+        if (!(n <= 0)) {
             System.out.println(n + " is not a negative number");
         }
-        String result = "";
+        String result = "-";
         n = -n;
-        result += "-";
-
-        while (n >= 10) {
+        while (n > 0) {
             result += n % 10;
             n /= 10;
         }
-        String intReturn = result + n;
-        int last = Integer.parseInt(intReturn);
-        return last;
+        return Integer.parseInt(result);
+    }
+    private static int reverseNegativeNum(int num) {
+        if (!(num <= 0)) {
+            System.out.println(num + " is not a negative number");
+        }
+        num = -num;
+        String numS = String.valueOf(num);
+        String reverse = "";
+        for (int i = numS.length() - 1; i >= 0; i--) {
+            reverse += numS.charAt(i);
+        }
+        num = Integer.parseInt(reverse);
+        return -num;
     }
     }
--- src/EmmaTasks/GroupStudying/tubish/Task8_Prime_Num.java
+++ src/EmmaTasks/GroupStudying/tubish/Task8_Prime_Num.java
@@ -2,18 +2,19 @@ package EmmaTasks.GroupStudying.tubish;
 
 public class Task8_Prime_Num {
 
-
+// 2 3 5 7  only prime digits and all the other numbers are made out multiplication prime numbers
 
     public static void primeNum(int n){
         if(n%2!=0 && n%3!=0 && n%5!=0 && n%7!=0){
             System.out.println("number " + n + " is a prime number");
         }else{System.out.println("number " + n + " is not a prime number");}
-
     }
 
     public static void main(String[] args) {
         primeNum(75);
-        primeNum(51);
+        primeNum(17);
+        primeNum(51);  //17*3
         primeNum(53);
+        primeNum(101);
     }
 }
