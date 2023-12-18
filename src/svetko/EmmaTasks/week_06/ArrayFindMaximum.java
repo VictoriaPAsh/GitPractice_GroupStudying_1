b0e7c198970af861172b68c664369e540a325f64
svetko <ssadmitrovic@gmail.com>
2023-12-17 20:52:38 -0500

    Updates

--- src/svetko/EmmaTasks/week_06/ArrayFindMaximum.java
+++ src/svetko/EmmaTasks/week_06/ArrayFindMaximum.java
@@ -2,17 +2,13 @@ package svetko.EmmaTasks.week_06;
 
 public class ArrayFindMaximum {
 
-
     public static void main(String[] args) {
-
 
         int[] arr = {3, 59, 14, 3, 4, 9, 7, 18};
 
         arrayMax(arr);
 
-
     }
-
 
     public static void arrayMax(int[] arr) {
 
@@ -21,14 +17,12 @@ public class ArrayFindMaximum {
         for (int i = 0; i < arr.length; i++) {
 
             if (arr[i] > max) {
-
                 max = arr[i];
             }
 
         }
 
         System.out.println("Maximum number is: " + max);
-
 
     }
 
