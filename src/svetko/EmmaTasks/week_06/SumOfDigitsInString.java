fc5f76ce4d46e3fa2e60cf3f794fa812ced5638a
svetko <ssadmitrovic@gmail.com>
2023-12-20 11:32:38 -0500

    Some tasks update

--- src/svetko/EmmaTasks/week_06/SumOfDigitsInString.java
+++ src/svetko/EmmaTasks/week_06/SumOfDigitsInString.java
@@ -13,17 +13,17 @@ public class SumOfDigitsInString {
 
     }
 
-    public static void sumOfDigits(String str){
+    public static void sumOfDigits(String str) {
 
         int sum = 0;
 
         List<String> list = new ArrayList<>();
 
-        for (int i=0; i <str.length() ; i++) {
+        for (int i = 0; i < str.length(); i++) {
 
             char each = str.charAt(i);
 
-            if(Character.isDigit(each)){
+            if (Character.isDigit(each)) {
                 list.add(each + "");
             }
 
@@ -31,14 +31,14 @@ public class SumOfDigitsInString {
 
         for (int i = 0; i < list.size(); i++) {
 
-            sum+=Integer.parseInt(list.get(i));;
+            sum += Integer.parseInt(list.get(i));
+            ;
         }
 
         System.out.println("sum = " + sum);
 
 
     }
-
 
 
 }
