e488cab3a5c0d1c65a998a47cad8d6ae1a2cc18b
svetko <ssadmitrovic@gmail.com>
2023-12-22 14:17:50 -0500

    Week 7 tasks solved

--- src/svetko/EmmaTasks/week_07/ArraySortAscending.java
+++ src/svetko/EmmaTasks/week_07/ArraySortAscending.java
@@ -13,16 +13,15 @@ public class ArraySortAscending {
         System.out.println(Arrays.toString(sorted));
 
 
-
     }
 
     public static int[] sortAscending(int[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
 
-            for (int j = 0; j < arr.length-1; j++) {
+            for (int j = 0; j < arr.length - 1; j++) {
 
-                if (arr[j] > arr[j+1]) {
+                if (arr[j] > arr[j + 1]) {
 
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
