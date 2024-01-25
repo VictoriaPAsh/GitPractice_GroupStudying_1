90007320d01a7a84e9f90775f223ad0fe247f7e9
svetko <ssadmitrovic@gmail.com>
2024-01-25 12:52:33 -0500

    Week 10 tasks solved

--- src/svetko/EmmaTasks/week_10/SortTheMap.java
+++ src/svetko/EmmaTasks/week_10/SortTheMap.java
@@ -2,7 +2,7 @@ package svetko.EmmaTasks.week_10;
 
 import java.util.*;
 
-public class SortTheMap{
+public class SortTheMap {
 
     public static void main(String[] args) {
 
@@ -15,48 +15,84 @@ public class SortTheMap{
         map.put("N6", 3);
 
         System.out.println("Unsorted = " + map);
+        System.out.println();
+
+        System.out.println("Sorted 1 = " + sortByValue(map));
+        //System.out.println("Sorted 2 = " + sortByValue2(map));
 
 
-        List<Integer> numbers = new ArrayList<>();
-        List<String> keys = new ArrayList<>();
+    }
 
-        for (String eachKey : map.keySet()) {
-            keys.add(eachKey);
+
+    public static Map<String, Integer> sortByValue(Map<String, Integer> map) {
+
+        List<Map.Entry<String, Integer>> list = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> eachSet : map.entrySet()) {
+            list.add(eachSet);
+
         }
 
+        for (int i = 0; i < list.size(); i++) {
 
-        for (Integer eachValue : map.values()) {
-            numbers.add(eachValue);
-        }
+            for (int j = 0; j < list.size() - 1; j++) {
 
-        for (int i = 0; i < numbers.size(); i++) {
+                if (list.get(j).getValue() > list.get(j + 1).getValue()) {
 
-            for (int j = 0; j < numbers.size() - 1; j++) {
-
-                if (numbers.get(j) > numbers.get(j + 1)) {
-
-                    int temp = numbers.get(j);
-                    numbers.set(j, numbers.get(j + 1));
-                    numbers.set(j + 1, temp);
-
+                    Map.Entry<String, Integer> temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
                 }
             }
         }
 
         map.clear();
 
-        for (int i = 0; i < numbers.size(); i++) {
-
-           map.put(keys.get(i),numbers.get(i));
-
+        for (Map.Entry<String, Integer> eachSet : list) {
+            map.put(eachSet.getKey(), eachSet.getValue());
         }
 
+        return map;
+    }
 
-        System.out.println("Sorted map = " + map);
+    public static Map<String, Integer> sortByValue2(Map<String, Integer> map) {
 
+        List<Integer> values = new ArrayList<>();
+        List<String> keys = new ArrayList<>();
 
+        for (String eachKey : map.keySet()) {
+            keys.add(eachKey);
+        }
+
+        for (Integer eachValue : map.values()) {
+            values.add(eachValue);
+        }
+
+        for (int i = 0; i < values.size(); i++) {
+
+            for (int j = 0; j < values.size() - 1; j++) {
+
+                if (values.get(j) > values.get(j + 1)) {
+
+                    int temp = values.get(j);
+                    values.set(j, values.get(j + 1));
+                    values.set(j + 1, temp);
+                }
+            }
+        }
+        map.clear();
+
+        for (int i = 0; i < values.size(); i++) {
+
+            map.put(keys.get(i), values.get(i));
+
+        }
+        return map;
     }
 }
+
+
+
 
 
 
